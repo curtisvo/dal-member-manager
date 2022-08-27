c8664107fea95eb33b5dcae811fc5bdad4c858d1
Curtis van Osch <curtisvo@gmail.com>
2022-08-27 11:49:48 -0400

    attempt to access the employe list using a session cookie

--- members-manager.py
+++ members-manager.py
@@ -6,5 +6,13 @@ config = configparser.ConfigParser()
 config.read('example.ini')
 config.read('config.ini') #overwrite values if config.ini is defined
 
-for key in config['Comelin']:
-    print(key+"="+config['Comelin'][key])
+loginResponse = requests.get('https://epiceriedal.comelin.com/admin-login?username=')
+if (loginResponse):
+    print('success')
+
+cookies = loginResponse.cookies
+print(cookies)
+
+emplListResponse = requests.get('https://epiceriedal.comelin.com/admin/employe/list', cookies=cookies)
+if (emplListResponse):
+    print(emplListResponse.text)
