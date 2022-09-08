680aa03327e36facfa39ed84a98e30bf87a2c9c9
Curtis van Osch <curtisvo@gmail.com>
2022-09-08 10:58:39 -0400

    use a different login url that seems to actually work

--- members-manager.py
+++ members-manager.py
@@ -6,18 +6,31 @@ config = configparser.ConfigParser()
 config.read('example.ini')
 config.read('config.ini') #overwrite values if config.ini is defined
 
-params = {'username': config['Comelin']['username'], 'password': config['Comelin']['password']}
-loginResponse = requests.get('https://epiceriedal.comelin.com/admin-login', params=params)
-if (loginResponse):
+s = requests.Session()
+
+#params = {'username': config['Comelin']['username'], 'password': config['Comelin']['password']}
+#loginResponse = s.get('https://epiceriedal.comelin.com/admin-login', params=params)
+#if (loginResponse):
+#    print('success')
+
+data={
+    'ctl00$cp$txtPassword':config['Comelin']['password'], 
+    'ctl00$cp$cmdConnect':'Se+connecter', 
+    '__VIEWSTATE': '/wEPDwUKLTgzMjE2ODE0MGRk0Hac/nPOyUDk/IkgIF7fT2dmNhgMCji7TXeZz3xhxds=', 
+    '__VIEWSTATEGENERATOR': '82312306', 
+    '__EVENTVALIDATION': '/wEdAAPgv4UYYoNz0vSrmOQVDw6gv/Q6v4a8EcF0W8X+HyR3nQio+wlyS62GHXUS+QQQsXRBaLk+Ri+KeDLS2UBV8HbFw0ufEBh3FQw6VoPc8qcs/A=='}
+    
+loginResponse2 = s.post('https://epiceriedal.comelin.com/admin/login.aspx', data=data)
+if (loginResponse2):
     print('success')
 
-cookies = loginResponse.cookies
-print(cookies)
+print(s.cookies)
 
-custListResponse = requests.get('https://epiceriedal.comelin.com/Api/Customers', cookies=cookies)
+custListResponse = s.get('https://epiceriedal.comelin.com/Api/Customers')
 if (custListResponse):
     print(custListResponse.text)
 
-emplListResponse = requests.get('https://epiceriedal.comelin.com/admin/employe/list', cookies=cookies)
+emplListResponse = s.get('https://epiceriedal.comelin.com/admin/employe/list')
 if (emplListResponse):
+    print(emplListResponse.status_code)
     print(emplListResponse.text)
