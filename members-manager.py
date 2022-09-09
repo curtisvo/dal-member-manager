7f52bba757f74a34048f4aa0973695d43bbece69
Curtis van Osch <curtisvo@gmail.com>
2022-09-09 16:11:21 -0400

    revert back to the api login url, returns the right cookies now

--- members-manager.py
+++ members-manager.py
@@ -8,20 +8,9 @@ config.read('config.ini') #overwrite values if config.ini is defined
 
 s = requests.Session()
 
-#params = {'username': config['Comelin']['username'], 'password': config['Comelin']['password']}
-#loginResponse = s.get('https://epiceriedal.comelin.com/admin-login', params=params)
-#if (loginResponse):
-#    print('success')
-
-data={
-    'ctl00$cp$txtPassword':config['Comelin']['password'], 
-    'ctl00$cp$cmdConnect':'Se+connecter', 
-    '__VIEWSTATE': '/wEPDwUKLTgzMjE2ODE0MGRk0Hac/nPOyUDk/IkgIF7fT2dmNhgMCji7TXeZz3xhxds=', 
-    '__VIEWSTATEGENERATOR': '82312306', 
-    '__EVENTVALIDATION': '/wEdAAPgv4UYYoNz0vSrmOQVDw6gv/Q6v4a8EcF0W8X+HyR3nQio+wlyS62GHXUS+QQQsXRBaLk+Ri+KeDLS2UBV8HbFw0ufEBh3FQw6VoPc8qcs/A=='}
-    
-loginResponse2 = s.post('https://epiceriedal.comelin.com/admin/login.aspx', data=data)
-if (loginResponse2):
+params = {'username': config['Comelin']['username'], 'password': config['Comelin']['password']}
+loginResponse = s.get('https://epiceriedal.comelin.com/admin-login', params=params)
+if (loginResponse):
     print('success')
 
 print(s.cookies)
